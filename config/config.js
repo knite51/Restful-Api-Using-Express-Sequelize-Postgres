5de0364da5566624d968dd59ccafa0db20964648
KniteDev <bmose@atbtechsoft.com>
2019-03-15 15:31:13 +0100

    add license and refactor keyNames

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     url: process.env.DEV_DB_URL,
-    dialet: 'postgres',
+    dialect: 'postgres',
     operatorsAliases: false
   }
 };
